023d9137c9359507faca3debc19638585bd24635
SnehaKallem <117821869+SnehaKallem@users.noreply.github.com>
2023-05-29 16:33:20 +0530

    Update settings.py

--- QP5 - Copy/QP3/settings.py
+++ QP5 - Copy/QP3/settings.py
@@ -133,6 +133,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'naveenrohit80830@gmail.com'
+EMAIL_HOST_USER = 'abcd80830@gmail.com'
 EMAIL_HOST_PASSWORD = 'aqdawbejpkaltoaq'
 BASE_URL = 'http://localhost:8000'
